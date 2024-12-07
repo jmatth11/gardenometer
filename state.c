5f9258ba98ad20138dafd42f8172f888dde5b8ea
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:18:21 -0600

    setting up simple state with arduino

---
+++ state.c
@@ -0,0 +1,17 @@
+#include "state.h"
+
+void handle_state_machine(state_machine_t *sm, void *context) {
+  switch(sm->state) {
+    case STATUS:
+      sm->status(sm, context);
+      break;
+    case CALIBRATION:
+      sm->calibration(sm, context);
+      break;
+    case ERROR:
+      sm->error(sm, context);
+      break;
+    default:
+      break;
+  }
+}
