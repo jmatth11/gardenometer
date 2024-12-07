217d080e43e49534327dd2c8024aa70e96a22ff3
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:18:28 -0600

    fixed missing functions and duplicate implementations

--- serial_parse.cpp
+++ serial_parse.cpp
@@ -1,10 +1,16 @@
 #include "serial_parse.h"
 
+
+const char *code_prefix = "code:";
+const char *config_prefix = "config:";
+const char *cal_prefix = "cal:";
+const char *status_prefix = "status:";
+
 void parse_serial(state_machine_t* sm, struct state* state, String data) {
   if (data.startsWith(code_prefix)) {
     String code = data.substring(strlen(code_prefix));
     int code_value = code.toInt();
-    sm->state = code_value;
+    sm->state = int_to_garden_state_t(code_value);
   } else if (data.startsWith(config_prefix)) {
     sm->state = CONFIG;
     state->serial_data = data.substring(strlen(config_prefix));
@@ -25,94 +31,94 @@ struct parse_info parse_value(String data) {
   struct parse_info result;
   result.starting_idx = idx;
   result.value = parsedValue;
-  if (idx != data.length()) {
+  if ((unsigned int)idx != data.length()) {
     result.starting_idx++;
   }
   return result;
 }
 
-struct config parse_config(String data) {
-  String err_msg = "data format error";
-  struct config local;
+String parse_config(String data, struct config* out) {
+  String err_msg;
   struct parse_info result;
   while (data.length() > 0) {
-    config_index idx = atoi(data.charAt(0));
+    int idx = atoi(data.substring(0, 1).c_str());
     switch(idx) {
       case WAIT_INDEX: {
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.wait_time = result.value;
+          out->wait_time = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case MOISTURE_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.moisture_pin = parse_value(data);
+          out->moisture_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case LUX_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.lux_pin = result.value;
+          out->lux_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case TEMP_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.temp_pin = result.value;
+          out->temp_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case CAL_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.cal_pin = result.value;
+          out->cal_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case ERR_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.err_pin = result.value;
+          out->err_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       case GOOD_INDEX:{
         if (data.charAt(1) == '=') {
           result = parse_value(data);
-          local.good_pin = result.value;
+          out->good_pin = result.value;
           data = data.substring(result.starting_idx);
         } else {
-          publish_error(err_msg + " code:" + idx);
+          err_msg = err_msg + "error for code:" + idx + "; ";
         }
         break;
       }
       default:{
-        publish_error(err_msg + " code:" + idx);
+        err_msg = err_msg + "error for code:" + idx + "; ";
         break;
       }
     }
   }
+  return err_msg;
 }
 
